59d53af3b10c8892fc959cbbff1aae106ef06a94
zatzk <zatzk@outlook.com>
2024-05-03 21:44:57 -0300

    chore: use localStorage to store colors

--- src/components/colorContext.tsx
+++ src/components/colorContext.tsx
@@ -2,7 +2,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unsafe-return */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
-import { createContext, useState, useContext } from "react";
+import { createContext, useState, useContext, useEffect } from "react";
 import { colorSets } from "./style/colors";
 
 interface ColorSet {
@@ -14,7 +14,23 @@ interface ColorSet {
 const ColorContext = createContext<any>(undefined);
 
 export function ColorProvider({ children }: { children: React.ReactNode }) {
-  const [activeColorSet, setActiveColorSet] = useState<ColorSet>(colorSets.blue);
+  const [activeColorSet, setActiveColorSet] = useState<ColorSet>(
+    () => {
+      // Retrieve initial value from localStorage (if available)
+      const storedColorSet = localStorage.getItem("activeColorSet");
+      if (storedColorSet) {
+        return JSON.parse(storedColorSet);
+      }
+      return colorSets.sky; // Default value
+    }
+  );
+
+  useEffect(() => {
+    if(typeof window !== "undefined") {
+    localStorage.setItem("activeColorSet", JSON.stringify(activeColorSet));
+    }
+  }, [activeColorSet]);
+
   return (
     <ColorContext.Provider value={{ activeColorSet, setActiveColorSet }}>
       {children}
