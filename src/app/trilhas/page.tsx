5715627a108090f686fb0cdc013e6701a0750797
zatzk <zatzk@outlook.com>
2024-05-03 21:44:17 -0300

    chore: add pages

---
+++ src/app/trilhas/page.tsx
@@ -0,0 +1,21 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+'use client'
+import { Inter } from "next/font/google";
+import { useColorContext } from "../../components/colorContext"
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
+
+
+export default function Trilhas() {
+  const {activeColorSet} = useColorContext();
+
+  return (
+    <section className={`font-sans ${inter.variable}  flex min-h-screen flex-col items-center justify-center `}>
+      <h1 className={`${activeColorSet?.secondary}`}>Trilhas</h1>
+    </section>
+  );
+}
