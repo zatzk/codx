cc08b175aec8a94a63e251706196b1b70c4ca1cf
zatzk <zatzk@outlook.com>
2024-06-02 10:48:39 -0300

    feat: add simplePageHeader, test

--- src/app/questoes/page.tsx
+++ src/app/questoes/page.tsx
@@ -2,7 +2,8 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 'use client'
 import { Inter } from "next/font/google";
-import { useColorContext } from "../../components/colorContext"
+import { useColorContext } from "../../components/style/colorContext"
+import { SimplePagHeader } from "~/components/simplePageHeader";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -14,8 +15,9 @@ export default function Questoes() {
   const {activeColorSet} = useColorContext();
 
   return (
-    <section className={`font-sans ${inter.variable}  flex min-h-screen flex-col items-center justify-center `}>
-      <h1 className={`${activeColorSet?.secondary}`}>Questões</h1>
+    <section className={`font-sans ${inter.variable} ${activeColorSet?.secondary} flex min-h-screen w-full flex-col items-center mt-20 `}>
+      <SimplePagHeader title="Questões" description="Quizzes to help you test and improve your knowledge and skill up" />
+      <h1 className={``}>Teste</h1>
     </section>
   );
 }
