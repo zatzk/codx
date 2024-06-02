919994f4e75851b32afe93e4be89f236c2d0889b
zatzk <zatzk@outlook.com>
2024-06-02 10:47:45 -0300

    style: update background

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import "~/styles/globals.css";
-import { ColorProvider } from "~/components/colorContext";
+import { ColorProvider } from "~/components/style/colorContext";
 
 export const metadata = {
   title: "CodX",
@@ -17,7 +17,13 @@ export default function RootLayout({
     <ColorProvider>
       <html lang="en">
         <body className={`relative h-full w-screen bg-slate-950`}>
-          <div className="absolute w-full h-screen bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]"></div>
+          <div 
+            className="
+              absolute w-full h-screen bottom-0 left-0 right-0 top-0 
+              bg-[linear-gradient(to_right,#3d3d3d2d_1px,transparent_1px),linear-gradient(to_bottom,#3d3d3d2d_1px,transparent_1px)] 
+              bg-[size:34px_34px] 
+              [mask-image:radial-gradientellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]
+              "/>
             {children}
         </body>
       </html>
