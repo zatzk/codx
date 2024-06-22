d3026b4b41807703f12e63aee4f8e3901a08c929
zatzk <zatzk@outlook.com>
2024-06-22 10:49:49 -0300

    feat: question page

--- src/app/questoes/[id]/page.tsx
+++ src/app/questoes/[id]/page.tsx
@@ -1,26 +1,47 @@
-/* eslint-disable @typescript-eslint/prefer-optional-chain */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-floating-promises */
 /* eslint-disable @typescript-eslint/no-unsafe-argument */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
-/* eslint-disable @typescript-eslint/restrict-template-expressions */
 // app/questoes/[id]/page.tsx
 'use client'
 import { useEffect, useState } from 'react';
 import { useParams } from 'next/navigation';
+import { QuestionList } from '~/components/questionComponents/questionList';
+import { QuestionLoader } from '~/components/questionComponents/questionLoader';
+import Link from 'next/link';
+import { useColorContext } from "~/lib/colorContext";
+import { Inter } from 'next/font/google';
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
 
 export default function Quizz() {
-  const params = useParams<{name: string}>();
+  const params = useParams<{id: string}>();
+  const name = params.id;
 
-  const name = params.name;
+  const {activeColorSet} = useColorContext();
 
-  interface Questao {
+  interface Question {
+    id: number;
     question: string;
     answer: string;
     topics: string[];
+    questionGroupId: number;
+  }
+
+  interface QuestionGroup {
+    id: number;
+    name: string;
+    title: string;
+    description: string;
+    questions: Question[];
   }
   
+  console.log('id:', name);
   
-  const [questao, setQuestao] = useState<Questao | null>(null);
+  const [questionGroup, setQuestionGroup] = useState<QuestionGroup  | null>(null);
 
   useEffect(() => {
     if (name) {
@@ -31,30 +52,37 @@ export default function Quizz() {
             throw new Error('Network response was not ok');
           }
           const data = await response.json();
-          setQuestao(data);
+          setQuestionGroup(data);
         } catch (error) {
           console.error('Failed to fetch questao:', error);
         }
       }
-      console.log('fetching questao:', name);
       fetchQuestao();
     }
   }, [name]);
-
-  if (!questao) {
-    return <div className='text-white'>Loading...</div>;
+  if (!questionGroup) {
+    return <QuestionLoader />;
   }
 
   return (
-    <div className='text-white'>
-      <h1>{name}</h1>
-      {/* {questao.questions && questao.questions.map(question => (
-        <div key={question.id}>
-          <h2>{question.question}</h2>
-          <p>{question.answer}</p>
-        </div>
-      ))} */}
+  <section className={`font-sans ${inter.variable} flex w-full flex-col items-center mt-28 text-white`}>
+    <div className="flex flex-col items-center mb-8">
+      
+      <Link href={'../questoes'} className='mb-4 mt-4 text-sm flex items-center text-gray-400 hover:text-white transition ease-in-out delay-50'>
+      <span
+        className='mr-2 mb-[2px] inline-block transform transition-transform group-hover:translate-x-[-2px]'
+      >
+        &larr;
+      </span>
+        <span>Voltar as questões</span> 
+      </Link>
+      <h1 className='text-5xl font-bold mb-4 mt-4'>{name} Quizz</h1>
+      <p className='text-lg font-light mt-3'>Teste, avalie e melhore seu conhecimento com estas perguntas.</p>
     </div>
+    <div className="mt-4">
+      <QuestionList questionGroup={questionGroup} />
+    </div>
+  </section>
   );
 };
 
