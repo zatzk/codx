b2c60979f4d8df1abdc622a0cac1b00ee1f68223
zatzk <zatzk@outlook.com>
2024-07-01 08:36:06 -0300

    chore: now gridItem receive route

--- src/components/gridItem.tsx
+++ src/components/gridItem.tsx
@@ -4,11 +4,11 @@
 import Link from "next/link"
 import { useColorContext } from "~/lib/colorContext";
 
-export function GridItem({ name }: { name: string }) {
+export function GridItem({ name, route }: { name: string, route: string }) {
   const {activeColorSet}: {activeColorSet: {cardBg: string}} = useColorContext();
 
   return (
-    <Link href={`/questoes/${name}`}
+    <Link href={`/${route}/${name}`}
       className={`${activeColorSet?.cardBg} hover:bg-opacity-30 bg-opacity-20 rounded-md h-24 w-[360px] flex items-center`}>
       <h1 className={`text-white text-md ml-5`}>{name}</h1>
     </Link>
