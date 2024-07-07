ba7b06361063347e05ed91b2693c0aa51d6c7189
zatzk <zatzk@outlook.com>
2024-07-07 14:53:31 -0300

    style: fix width

--- src/components/simplePageHeader.tsx
+++ src/components/simplePageHeader.tsx
@@ -7,7 +7,7 @@ interface SimplePagHeaderProps {
 export function SimplePagHeader( props: SimplePagHeaderProps ) {
 
   return (
-    <div className="w-full pt-6 pb-5 sm:pt-12 sm:pb-10">
+    <div className="w-2/3 pt-6 pb-5 sm:pt-12 sm:pb-10">
       <div className="max-w-[830px] mx-auto px-[1rem]">
       <h1 className="text-white text-3xl sm:text-5xl mb-3 sm:mb-4 font-bold">{props.title}</h1>
       <p className="text-gray-400 ml-1 text-lg">{props.description}</p>
