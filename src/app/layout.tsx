4ed41971c1c0767d242e94e2c37eb44f4f48a9c7
zatzk <zatzk@outlook.com>
2024-07-09 21:36:05 -0300

    chore: remove commented code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,15 +16,8 @@ export default function RootLayout({
   return (
     <ColorProvider>
       <html lang="en">
-        <body className={`relative h-full w-screen bg-slate-950`}>
-          {/* <div 
-            className="
-              absolute w-full h-screen bottom-0 left-0 right-0 top-0 z-10
-              bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] 
-              bg-[size:34px_34px] 
-              [mask-image:radial-gradient(ellipse_60%_50%_at_50%_100%,#000_70%,transparent_100%)]
-              "/> */}
-            {children}
+        <body className={`relative h-[1000px] w-screen bg-slate-950`}>
+          {children}
         </body>
       </html>
     </ColorProvider>
