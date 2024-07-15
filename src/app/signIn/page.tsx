e5ca3a28d653bbee3780b206e48ca3d2e77eefdd
zatzk <zatzk@outlook.com>
2024-07-14 21:12:23 -0300

    feat: signIn page

---
+++ src/app/signIn/page.tsx
@@ -0,0 +1,12 @@
+// src/app/signIn/page.tsx
+import React from 'react'
+
+export default function SignIn() {
+  return (
+    <div>
+      <h1 className="">
+        SignIn
+      </h1>
+    </div>
+  )
+}
