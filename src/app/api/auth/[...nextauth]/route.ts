42945a6386e6a1480fa6f9a1e067000138f8a28c
zatzk <zatzk@outlook.com>
2024-07-14 21:22:21 -0300

    chore: fix next auth route

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,25 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
-import NextAuth, { type NextAuthOptions } from "next-auth";
-import GithubProvider from "next-auth/providers/github";
-// import GoogleProvider from "next-auth/providers/google";
-
-export const authOptions: NextAuthOptions = {
-  // Configure one or more authentication providers
-  providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_CLIENT_ID!,
-      clientSecret: process.env.GITHUB_SECRET_ID!,
-    }),
-    // GoogleProvider({
-    //   clientId: process.env.GOOGLE_CLIENT_ID!,
-    //   clientSecret: process.env.GOOGLE_SECRET_ID!,
-    // }),
-  ],
-  pages: {
-    signIn: "/signin",
-  },
-  secret: process.env.NEXTAUTH_SECRET!,
-};
+import NextAuth from 'next-auth'
+import { authOptions } from "~/lib/auth";
 
 const handler = NextAuth(authOptions);
 
