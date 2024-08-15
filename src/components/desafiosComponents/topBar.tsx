3fabe9a527b1d6cb3e8fde029abc400d60a87b5a
zatzk <zatzk@outlook.com>
2024-08-15 11:50:40 -0300

    feat: add desafios topbar

--- src/components/desafiosComponents/topBar.tsx
+++ src/components/desafiosComponents/topBar.tsx
@@ -1,11 +1,21 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+'use client';
 import React from 'react'
+import { useColorContext } from '~/lib/colorContext';
 
 interface TopBarProps {
   title: string;
 }
 
 export default function TopBar({title}: TopBarProps) {
+  const {activeColorSet} = useColorContext();
+
   return (
-    <div className='w-full flex items-center justify-center'>{title}</div>
+    <div className={`w-full flex items-center justify-start rounded-lg h-10 mb-3 `}>
+      <div className='ml-3'>
+        Desafios List
+      </div>
+    </div>
   )
 }
