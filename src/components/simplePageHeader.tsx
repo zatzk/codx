5079dddadbc934c27acbb66dd5e4ad45551f01d1
zatzk <zatzk@outlook.com>
2024-09-23 20:08:13 -0300

    feat: new arrow icon and back button logic

--- src/components/simplePageHeader.tsx
+++ src/components/simplePageHeader.tsx
@@ -12,17 +12,23 @@ const inter = Inter({
   variable: "--font-sans",
 });
 
+
+
 interface SimplePagHeaderProps {
   title: string
   description: string
 }
 export function SimplePagHeader( props: SimplePagHeaderProps ) {
 
+
   return (
     <div className={`w-2/3 border rounded-2xl px-3 py-3 ${silkscreen.className}`}>
       <div className="px-[1rem]">
-      <h1 className={`text-white text-md `}>{props.title}</h1>
-      <p className={`text-gray-400 text-sm ${inter.className}`}>{props.description}</p>
+        <div className="flex items-center mb-1">
+          <span onClick={() => window.history.back()} className="pixelarticons--arrow-left text-xl mr-2 cursor-pointer"></span>
+          <h1 className={`text-white text-md`}>{props.title}</h1>
+        </div>
+      <p className={`text-gray-400 text-xs ml-1 ${inter.className}`}>{props.description}</p>
       </div>
     </div>
   )
