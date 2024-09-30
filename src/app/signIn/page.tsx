994b9c3bf00f0c243e84a64c23566a1c8ce65cc0
zatzk <zatzk@outlook.com>
2024-09-30 08:34:45 -0300

    fix: occult unused button

--- src/app/signIn/page.tsx
+++ src/app/signIn/page.tsx
@@ -35,10 +35,10 @@ export default function SignIn() {
     <div className={`w-2/6 h-2/3 ${activeColorSet?.cardGradient} border ${activeColorSet?.borderButton} ${silkscreen.className} rounded-md text-white flex flex-col justify-center items-center`}>
         <p className="text-2xl font-bold mb-6">Sign In</p>
         <div className={`flex flex-col ${inter.className}`}>
-          <button className={`bg-blue-500 hover:bg-opacity-80 flex items-center py-2 px-5 rounded-md mb-2`} onClick={() => signIn('google')}>
+          {/* <button className={`bg-blue-500 hover:bg-opacity-80 flex items-center py-2 px-5 rounded-md mb-2`} onClick={() => signIn('google')}>
             <FaGoogle className="mr-3"/>
             <p>Continue with google</p>
-          </button>
+          </button> */}
           <button className={`bg-black hover:bg-slate-900 flex items-center border-gray-300 border py-2 px-5 rounded-md mb-2`} onClick={() => signIn('github')}>
             <FaGithub className="mr-3"/>
             Continue with github
